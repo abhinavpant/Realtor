a038bf7efbce71a124dcdb6a0ad186ec7377e7b8
abhinavpant <141048769+abhinavpant@users.noreply.github.com>
2024-02-03 23:34:59 +0530

    SignUp page functionality FINAL

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,8 @@ export default defineConfig({
   server:{
     proxy:{
       '/api': {
-        target: 'https://localhost:3000'
+        target: 'http://localhost:3000',
+        secure: false
       },
     },
   },
