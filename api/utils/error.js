64f16542c158115b6a738cd0c13f81416e04122b
abhinavpant <141048769+abhinavpant@users.noreply.github.com>
2024-02-04 09:39:01 +0530

    Sign in page functionality

--- api/utils/error.js
+++ api/utils/error.js
@@ -1,5 +1,5 @@
-export const errorhandler = (statusCode, message)=>{
-    const error = new Error()
+export const errorHandler = (statusCode, message)=>{
+    const error = new Error();
     error.statusCode = statusCode;
     error.message = message;
     return error;
