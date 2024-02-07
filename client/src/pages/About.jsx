99f1bcb8913b268ebe5edc397335606c6685d74b
abhinavpant <141048769+abhinavpant@users.noreply.github.com>
2024-02-07 14:15:32 +0530

    Complete Home page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 
-function About() {
+export default function About() {
   return (
-    <div>About</div>
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>About Mega Estate</h1>
+      <p className='mb-4 text-slate-700'>At Mega Estate, we understand that finding the perfect property is not just about a physical space; it's about discovering a place to call home, a space to grow your dreams, and an investment in your future. Whether you are searching for your dream home, a lucrative investment opportunity, or looking to sell your property, we are here to guide you every step of the way.</p>
+      <p className='mb-4 text-slate-700'>
+      Our mission is to help our clients achieve their real estate goals by providing expert advice, personalized service, and a deep understanding of the local market. Whether you are looking to buy, sell, or rent a property, we are here to help you every step of the way.
+      </p>
+      <p className='mb-4 text-slate-700'>Your satisfaction is our priority. We pride ourselves on our client-centric approach, ensuring that your unique needs and preferences are at the forefront of every transaction. Our goal is to make your real estate experience seamless and enjoyable.</p>
+      <p className='mb-4 text-slate-700'>Our team of agents has a wealth of experience and knowledge in the real estate industry, and we are committed to providing the highest level of service to our clients. We believe that buying or selling a property should be an exciting and rewarding experience, and we are dedicated to making that a reality for each and every one of our clients.</p>
+    </div>
   )
 }
-
-export default About
