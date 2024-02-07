22de62631a191281c7e01824424bffb75a113ac2
abhinavpant <141048769+abhinavpant@users.noreply.github.com>
2024-02-07 15:11:13 +0530

    final

--- api/index.js
+++ api/index.js
@@ -8,9 +8,7 @@ import cookieParser from 'cookie-parser';
 import path from 'path';
 dotenv.config();
 
-mongoose
-  .connect(process.env.MONGO)
-  .then(() => {
+mongoose.connect(process.env.MONGO).then(() => {
     console.log('Connected to MongoDB!');
   })
   .catch((err) => {
